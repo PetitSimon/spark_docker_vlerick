e4c9e9c9090e0b23c086b97bbf611e89e2295749
PetitSimon <simpetit1@gmail.com>
2022-12-12 18:16:52 +0000

    save

--- Assignment/ETL-project.py
+++ Assignment/ETL-project.py
@@ -39,41 +39,6 @@ import networkx as nx
 import itertools
 import matplotlib.pyplot as plt
 
-df.rename(columns = {'actor_2_facebook_likes;':'actor_2_facebook_likes'}, inplace = True)
-df['actor_2_facebook_likes'] = df['actor_2_facebook_likes'].replace(';','', regex=True)
-df['actor_2_facebook_likes'].replace('', np.nan, inplace = True) 
-df.actor_2_facebook_likes = df.actor_2_facebook_likes.astype(float, errors = 'ignore')
-
-
-# In[7]:
-
-
-df1 = pd.read_csv("../Data/after_release.csv")
-df1.head()
-
-
-# In[8]:
-
-
-df1.shape
-
-
-# In[9]:
-
-
-df1.info()
-
-
-# In[10]:
-
-
-df1.describe().round(2)
-
-
-# The target variable imdb_score column is merged to the pre-sale dataset.
-
-# In[11]:
-
 
 df_original = df
 df_name_col = df[['director_name', 'actor_3_name', 'actor_2_name', 'actor_1_name', 'movie_title']]
@@ -114,24 +79,9 @@ plt.pie(language_count.values, labels=language_count.index,
        autopct='%.0f%%');
 
 
-# In[15]:
-
-
-df[df.language == 'Dutch']
-
-
-# Looking into the country of the movies in the dataset and 73% of the movies are made in the USA.
 
 # In[16]:
 
-
-df['country'].value_counts()
-
-
-# In[17]:
-
-
-df[df.country == 'Belgium']
 
 
 # We must be proud of the only Belgian movie that is in the dataset!
@@ -147,28 +97,10 @@ plt.pie(country_count.values, labels=country_count.index,
 # In[19]:
 
 
-plt.figure(figsize=(10,8))
-
-movie20_df= df_copy.sort_values(by ='imdb_score' , ascending=False)
-movie_df_new=movie20_df.head(20)
-ax=sns.pointplot(movie_df_new['director_name'], movie_df_new['imdb_score'], hue=movie_df_new['movie_title'])
-ax.set_xticklabels(ax.get_xticklabels(), rotation=40, ha="right")
-plt.tight_layout()
-plt.show()
 
 
-# When looking at the 20 best rated movies I observe that I don't recognize a movie that is in the imdb top 20 on the website. This means that a lot of data is missing compared to the official IMDB list.
 
-# # Data preparation
 
-# Overview of the steps to prepare the data:
-# - Missing Values
-# - Duplicates
-# - Outliers
-
-# ### Missing values
-
-# #### Pre release dataframe
 
 # In[20]:
 
@@ -206,14 +138,6 @@ df.isnull().sum()
 df[df.isnull().any(axis = 1)].shape
 
 
-# ### Duplicate rows
-
-# #### Pre release dataframe
-
-# duplicate rows for the pre release dataframe
-
-# In[25]:
-
 
 #duplicated rows
 df[df.duplicated() == True]
@@ -236,21 +160,13 @@ print(df[df.duplicated() == True].shape[0])
 
 
 #make a separate boxplot for every variable with the same metrics
-sns.boxplot(data= df['duration']);
-print("We observe no outliers in the duration of the movies")
+
 
 
 # In[28]:
 
 
-plt.xticks(rotation=90)
 
-sns.boxplot(data= df[['director_facebook_likes' ,
-                     'actor_1_facebook_likes',
-                     'actor_2_facebook_likes',
-                     'actor_3_facebook_likes',
-                     ]]);
-print("We observe no outliers in the Facebook likes")
 
 
 # In[29]:
@@ -262,14 +178,6 @@ print("We observe no outliers in the Facebook likes")
 
 # In[30]:
 
-
-sns.boxplot(data= df['budget']);
-print("We observe no outliers in the budget variable")
-
-
-# ## Preprocessing the data
-
-# Dummification of the "genres" variable
 
 # In[31]:
 
@@ -383,26 +291,7 @@ plt.xlabel('imdb_score', fontsize=12)
 plt.show()
 
 
-# In[42]:
 
-
-# most correlated features
-import seaborn as sns
-corrmat = df.corr()
-plt.figure(figsize = (25,7))
-top_corr_features = corrmat.index[abs(corrmat["imdb_score"])>0.1]
-g = sns.heatmap(df[top_corr_features].corr(),annot=True,cmap="RdYlGn")
-
-
-# In this colleration matrix, we observe the following:
-# There is high multicollinearity among the numerical variables associated with facebook likes. 
-# First, it can be observed that there is a logical relationship between actors' facebook likes and the entire cast. 
-# Second, there is a high correlation between the facebook likes of the different actors. 
-# This would then be a consequence that well-known actors are much more collaborative and less well-known actors are also more collaborative and vice versa. This could be a logical result as the cast of a successful film all get more facebook likes afterwards. In the back of your mind, it should be remembered that the films were all released in the past tense and the facebook likes is a snapshot of now.
-
-# To remove the multicollinearity a new row will replace cast_total_facebook_likes. A new column is made with the sum of the second and thirth actor of the movie.
-
-# In[43]:
 
 
 df['other_actor_facebook_likes'] = df['actor_3_facebook_likes'] + df['actor_2_facebook_likes']
@@ -423,23 +312,7 @@ df.drop('cast_total_facebook_likes',axis=1,inplace=True)
 # In[45]:
 
 
-#Simple Linear Regression => t_Price = B0 + B1t_BHP + e
-from sklearn.linear_model import LinearRegression
-model = LinearRegression(fit_intercept=True) 
-model.fit(df[['budget','director_facebook_likes']], df[['imdb_score']])
-print("Intercept or B0: ",model.intercept_)
-print("B1: ",model.coef_[0,0])
-print("B2: ",model.coef_[0,1])
 
-
-# In[46]:
-
-
-#Simple regression model 1
-import statsmodels.api as sm
-from statsmodels.formula.api import ols
-model1 = ols('imdb_score ~ budget + director_facebook_likes + actor_1_facebook_likes + other_actor_facebook_likes', data=df).fit()
-print(model1.summary())
 
 
 # ### Random forest
@@ -510,8 +383,9 @@ rsquare = r2_score(y_test, y_pred)
 mae = mean_absolute_error(y_test, y_pred)
 pd.DataFrame({'eval_criteria': ['r-square','MAE'],'value':[rsquare,mae]})
 
+val_pred = y_pred
 #######END---ML##########
 
 #step 4
-result = spark.createDataFrame(y_pred)
+result = spark.createDataFrame(val_pred)
 reult.show()
